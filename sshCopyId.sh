be1e8433810779b4e19ff53cf372aaa1841252f5
rkabrick <rkabrick@udel.edu>
2018-11-28 11:12:13 -0500

    SSH Key Copy Script

---
+++ sshCopyId.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Program: Copy All SSH Keys to All Nodes
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA01
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA02
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA03
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA04
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA05
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA06
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA07
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA08
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA09
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA10
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA11
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA12
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA13
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA14
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA15
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA16
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA17
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA18
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA19
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA20
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA21
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA22
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA23
+ssh-copy-id -i ~/.ssh/id_rsa parallella@NOPA24
